7ee3aea30dd97ee1b9a959cbdebd159f2a5819ff
Gustavo Gil Condezo Camayo +51 931572561 <gustavocondezocamayo@gmail.com>
2020-01-26 00:51:21 -0500

    desplegando pagina web

---
+++ index.js
@@ -0,0 +1,23 @@
+//importamos express
+const express = require("express");
+const app = express();
+
+
+//acrhivos estaticos
+app.use(express.static(__dirname + "/public"));
+//rutas
+app.get("/acerca", (req, res) => {
+    res.sendFile(__dirname + "/acerca.html");
+});
+app.get("/nosotros", (req, res) => {
+    res.sendFile(__dirname + "/nosotros.html");
+});
+
+
+app.get("*", (req, res) => {
+    res.send("No se encuentra esa pagina")
+});
+
+app.listen(3000, () => {
+    console.log("en puerto 30000");
+});
